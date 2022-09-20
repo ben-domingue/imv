bc219f65ef653ddd9a5b22740d35ee9c1572fb8f
ben-domingue <ben.domingue@gmail.com>
2022-09-20 06:05:00 -0700

    update

--- R/imvglm.R
+++ R/imvglm.R
@@ -3,10 +3,11 @@ imv0glm<-function(m,nfold=5) {
     x<-m$model
     if (nfold>nrow(x)) stop()
     x$group<-sample(1:nfold,nrow(x),replace=TRUE)
-    foldfun<-function(train,test) {
+    foldfun<-function(train,test,m) {
+        mm<-update(m,data=train)#glm(fm,train,family="binomial")
         fm<-m$formula
-        mm<-glm(fm,train,family="binomial")
-        m0<-glm(paste(all.vars(fm)[1],"~1"),train,family="binomial")
+        new.fm<-paste(all.vars(fm)[1],"~1")
+        m0<-update(m,formula.=as.formula(new.fm),data=train)#glm(,train,family="binomial")
         ##
         p0<-predict(m0,test,type="response")
         p1<-predict(mm,test,type="response")
@@ -18,7 +19,7 @@ imv0glm<-function(m,nfold=5) {
     for (i in 1:length(L)) {
         test<-L[[i]]
         train<-data.frame(do.call("rbind",L[-i]))
-        om[i]<-foldfun(train=train,test=test)
+        om[i]<-foldfun(train=train,test=test,m=m)
     }
     om
 }
