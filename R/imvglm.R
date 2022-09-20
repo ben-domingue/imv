fe483c1ba7dcb6c6b32c7d04fbedd4fe2cb56a4d
ben-domingue <ben.domingue@gmail.com>
2022-09-20 05:01:15 -0700

    imvglm

---
+++ R/imvglm.R
@@ -0,0 +1,52 @@
+
+imv0glm<-function(m,nfold=5) {
+    x<-m$model
+    if (nfold>nrow(x)) stop()
+    x$group<-sample(1:nfold,nrow(x),replace=TRUE)
+    foldfun<-function(train,test) {
+        fm<-m$formula
+        mm<-glm(fm,train,family="binomial")
+        m0<-glm(paste(all.vars(fm)[1],"~1"),train,family="binomial")
+        ##
+        p0<-predict(m0,test,type="response")
+        p1<-predict(mm,test,type="response")
+        om<-imv_binary(test[[all.vars(fm)[1] ]],p0,p1)
+        om
+    }
+    L<-split(x,x$group)
+    om<-numeric()
+    for (i in 1:length(L)) {
+        test<-L[[i]]
+        train<-data.frame(do.call("rbind",L[-i]))
+        om[i]<-foldfun(train=train,test=test)
+    }
+    om
+}
+
+
+imvglm<-function(m,nfold=5,
+                 var.nm #name of variable you want to remove
+                 ) {
+    x<-m$model
+    if (nfold>nrow(x)) stop()
+    x$group<-sample(1:nfold,nrow(x),replace=TRUE)
+    foldfun<-function(train,test) {
+        fm<-m$formula
+        mm<-glm(fm,train,family="binomial")
+        fm0<-update.formula(fm,as.formula(paste(".~. - ",var.nm,sep='')))
+        m0<-glm(fm0,train,family="binomial")
+        ##
+        p0<-predict(m0,test,type="response")
+        p1<-predict(mm,test,type="response")
+        om<-imv_binary(test[[all.vars(fm)[1] ]],p0,p1)
+        om
+    }
+    L<-split(x,x$group)
+    om<-numeric()
+    for (i in 1:length(L)) {
+        test<-L[[i]]
+        train<-data.frame(do.call("rbind",L[-i]))
+        om[i]<-foldfun(train=train,test=test)
+    }
+    om
+}
--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # imv
+See:
+https://osf.io/preprints/socarxiv/gu3ap/
+https://psyarxiv.com/q3djt/
+
+Examples
+
+```
+library(imv)
+x<-rnorm(1000)
+y<-rbinom(length(x),1,1/(1+exp(-x)))
+df<-data.frame(x=x,y=y)
+m<-glm(y~x,df,family="binomial")
+
+new.y<-rbinom(length(x),1,1/(1+exp(-x)))
+pr<-predict(m,data.frame(x=x),type='response')
+imv_binary(new.y,mean(y),pr) #imv for new set of outcomes
+
+imv0glm(m) #imv for full model versus null model
+
+df$z<-rnorm(nrow(df))
+m<-glm(y~x+z,df,family="binomial")
+#summary(m)
+imvglm(m,var.nm='z')
+```
